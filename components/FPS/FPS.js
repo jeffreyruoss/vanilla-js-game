ab8e541525b8de016df28f46a8a905f2ed44a1e1
Jeff Ruoss <jeff@zenthree.com>
2020-08-01 18:53:30 -0700

    Rework FPS

--- components/FPS/FPS.js
+++ components/FPS/FPS.js
@@ -3,44 +3,22 @@ export class FPS {
     this.createFPScontainer(game);
     this.trackFPS(game);
   }
-
   createFPScontainer(game) {
     this.FPSContainer = document.createElement("div");
     this.FPSContainer.setAttribute("id", "fps-container");
     this.FPSContainer.innerHTML = `
-    <table>
-      <tr>
-        <td><label>FPS (true): </label></td>
-        <td><span id="fps-true">0</span></td>
-      </tr>
-      <tr>
-        <td><label>FPS (game): </label></td>
-        <td><span id="fps-game">0</span></td>
-      </tr>
-    </table>
+    <label>FPS: </label>
+    <span id="fps-value">0</span>
     `;
     game.element.appendChild(this.FPSContainer);
-    game.FPStrue = document.getElementById("fps-true");
-    game.FPSgame = document.getElementById("fps-game");
+    game.FPSvalue = document.getElementById("fps-value");
   }
-
-  // NEXT
-  // Set the fps true and fps game element reference to game.fpstrue and game.fpsgame
-  // That way I might be able to use them within setInterval
 
   trackFPS(game) {
     let clockCurrent = 0;
     setInterval(function () {
-      game.FPStrue.innerText = game.clock - clockCurrent;
+      game.FPSvalue.innerText = game.clock - clockCurrent;
       clockCurrent = game.clock;
     }, 1000);
   }
-
-  /**
-   * Actual number of time requestAnimationFrame ran
-   */
-
-  /**
-   * Game FPS that we set
-   */
 }
