96435783e284862cce17de5b3a54435f1a37a5fc
Jeff Ruoss <jeff@zenthree.com>
2020-08-22 17:20:42 -0700

    Clean up pauseButton.

--- components/PauseButton/PauseButton.js
+++ components/PauseButton/PauseButton.js
@@ -1,53 +1,46 @@
 export class PauseButton {
   constructor(game) {
-    this.checkState();
+    this.pauseText = "PAUSE (⎵)";
+    this.unpauseText = "UNPAUSE (⎵)";
     this.createButton(game);
     this.addClickEvent();
-  }
-
-  checkState() {
-    if (localStorage.getItem("pauseState") === null) {
-      localStorage.setItem("pauseState", "unpaused");
-    }
+    this.addKeyDownEvent();
   }
 
   createButton(game) {
     this.element = document.createElement("button");
     game.world.element.appendChild(this.element);
-    if (localStorage.getItem("pauseState") === "unpaused") {
-      this.element.innerText = "PAUSE (⎵)";
-    } else {
-      this.element.innerText = "UNPAUSE (⎵)";
-    }
+    localStorage.setItem("pauseState", "unpaused");
+    this.element.innerText = this.pauseText;
     this.element.style.top = "80px";
     this.element.style.padding = "7px 10px";
   }
 
   addClickEvent() {
     this.element.addEventListener("click", () => {
-      if (localStorage.getItem("pauseState") === "paused") {
-        localStorage.setItem("pauseState", "unpaused");
-        this.element.innerText = "PAUSE (⎵)";
-      } else {
-        localStorage.setItem("pauseState", "paused");
-        this.element.innerText = "UNPAUSE (⎵)";
-      }
+      this.setPauseState();
     });
+  }
 
+  addKeyDownEvent() {
     document.addEventListener("keydown", (e) => {
       if (e.keyCode == 32) {
-        if (localStorage.getItem("pauseState") === "paused") {
-          localStorage.setItem("pauseState", "unpaused");
-          this.element.innerText = "PAUSE (⎵)";
-        } else {
-          localStorage.setItem("pauseState", "paused");
-          this.element.innerText = "UNPAUSE (⎵)";
-        }
+        this.setPauseState();
       }
     });
   }
 
+  setPauseState() {
+    if (localStorage.getItem("pauseState") === "paused") {
+      localStorage.setItem("pauseState", "unpaused");
+      this.element.innerText = this.pauseText;
+    } else {
+      localStorage.setItem("pauseState", "paused");
+      this.element.innerText = this.unpauseText;
+    }
+  }
+
   deconstructor() {
-    localStorage.setItem("pauseState", "unpaused");
+    localStorage.removeItem("pauseState");
   }
 }
