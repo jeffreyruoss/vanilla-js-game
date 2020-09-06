87ca579c5ca5bc7c0bda2f6481f4c7c4a4c682e2
Jeff Ruoss <jeff@zenthree.com>
2020-09-06 14:04:24 -0700

    Excluding the last frame of the spritesheets since it is a duplicate.

--- components/Sprite/SpriteAnimate.js
+++ components/Sprite/SpriteAnimate.js
@@ -1,7 +1,7 @@
 export const SpriteAnimate = {
     spriteAnimate(game, currentAnimation) {
         if (game.world.clock % game.world.spriteSheetSpeed == 0) {
-            if (parseInt(this.element.style.backgroundPositionX) == 384) {
+            if (parseInt(this.element.style.backgroundPositionX) == 320) {
               this.element.style.backgroundPositionX = 0;
             } else {
               this.element.style.backgroundPositionX =
